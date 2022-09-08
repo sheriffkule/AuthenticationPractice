a2e4c4a172c6110dc69dc151a29d4340519ca0bc
sheriffkule <zeljko225@yahoo.com>
2022-09-08 21:45:27 +0200

    accessing protected resources

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import axios from 'axios';
 import {StyleSheet, Text, View} from 'react-native';
+import {AuthContext} from '../store/auth-contex';
 
 function WelcomeScreen() {
+  const [fetcedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token();
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://expense-tracker-60f7d-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=' +
+          token,
+      )
+      .then(response => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetcedMessage}</Text>
     </View>
   );
 }
